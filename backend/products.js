7f4d8d23474bc9d091f0e55458ff05f4c47cc816
Nithin Roy <nithinroy182.com>
2024-12-14 02:00:30 +0530

    first

---
+++ backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/drrjn8yis/image/upload/v1733980933/aqy6to0d2rzgrj1oiwq2.png",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/drrjn8yis/image/upload/v1733980933/aqy6to0d2rzgrj1oiwq2.png",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/drrjn8yis/image/upload/v1733980933/aqy6to0d2rzgrj1oiwq2.png",
+  },
+];
+
+module.exports = products;
